14862673e12fb7917de735b49c5c5b0ff803625b
PanagiotisMenounosBLI <p.menounos@outlook.com>
2024-11-19 10:58:50 +0200

    remove runtime hook file

--- pyinstaller/pyi_rth_occ.py
+++
@@ -1,7 +0,0 @@
-from os import environ as env
-
-env['CASROOT'] = 'opencascade'
-
-env['CSF_ShadersDirectory'] = 'opencascade/src/Shaders'
-env['CSF_UnitsLexicon'] = 'opencascade/src/UnitsAPI/Lexi_Expr.dat'
-env['CSF_UnitsDefinition'] = 'opencascade/src/UnitsAPI/Units.dat'
